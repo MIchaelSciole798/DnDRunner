968b6772fb58f6080f0b5693b2b8882d0755f248
MIchaelSciole798 <81838472+MIchaelSciole798@users.noreply.github.com>
2021-11-26 13:11:02 -0500

    updated db.json

--- my-app/src/Components/CaramelList.js
+++ my-app/src/Components/CaramelList.js
@@ -7,9 +7,18 @@ export default class CaramelList extends React.Component {
     };
 
     componentDidMount() {
-        axios.get('').then(res => {
+        axios.get('https://my-json-server.typicode.com/MIchaelSciole798/DnDRunner/caramels').then(res => {
             console.log(res);
             this.setState({ caramels: res.data });
         });
+    }
+
+    render()
+    {
+        return (
+            <ul>
+                { this.state.caramels.map(caramel => <li>Type: {caramel.flavor}....Quantity per box: {caramel.quantity}....Price per box: {caramel.price}</li>)}
+            </ul>
+        )
     }
 }
